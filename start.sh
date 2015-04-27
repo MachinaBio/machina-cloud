b8c6c99da5821cdc83d3278c5f17b92553efb6e9
Skyler Brungardt <skyler.brungardt@gmail.com>
2015-04-27 13:36:47 -0700

    Modified docker start script.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker run -d -p 80:3000 --name machina-cloud -v `pwd`:/machina-cloud machinabio/machina-cloud:init
+sudo docker run -d -p 80:3000 --name machina-cloud -v `pwd`:/machina-cloud machinabio/machina-cloud:init-1.1.1
