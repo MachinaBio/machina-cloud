92976a9f78df0a9cfd13f7ed4d1f172927590990
luzlab <carlo@machina.bio>
2015-08-24 20:04:22 -0700

    Added mailchimp integration into the server side "addSubscriber" method.

--- server/collections/subscribers.js
+++ server/collections/subscribers.js
@@ -1,7 +1,21 @@
 Meteor.methods({
 
   'addSubscriber': function (email) {
+  	var mailingLists = new MailChimpLists("cfd8488160959200c417f5f936a1282e-us11", { version : '2.0' });
+  	mailingLists.subscribe({
+  		apikey: "cfd8488160959200c417f5f936a1282e-us11",
+  		email: { 
+  			email : email,
+  			},
+  		id: "d227f6b1ef"
+  		},
+  		function (error, results) {
+  			console.log("Error: ",error);
+  			console.log("Results: ",results);
+
+  	});
 
     Subscribers.upsert(email, { subscribed: true });
+    
   }
 });
